cae385dca71d8af0cd98ad454f4d032d6f985379
monicakhoe <monickhoe@yahoo.com>
2019-05-17 14:47:10 +0700

    frame animation (change size and background color)

--- frameExample/ViewController.swift
+++ frameExample/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var viewFrame: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UIView.animate(withDuration: 3, animations: {
+            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 80, height: 50)
+            self.viewFrame.backgroundColor = UIColor.blue
+        })
     }
 
 
