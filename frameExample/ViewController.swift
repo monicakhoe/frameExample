60481ed95993cb09615ec1822c9e7f858ef5bb8c
monicakhoe <monickhoe@yahoo.com>
2019-05-17 17:10:12 +0700

    added round button, color random, and animation random

--- frameExample/ViewController.swift
+++ frameExample/ViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var viewFrame: UIView!
+    
+    @IBOutlet weak var roundButton: UIButton!
+    var currentX = 50.0
+    var currentY = 100.0
+    
+    @IBAction func roundButton(_ sender: UIButton) {
+        self.currentX = Double(Int.random(in: 0 ... 300))
+        self.currentY = Double(Int.random(in: 0 ... 700))
+        roundButton.backgroundColor = UIColor.init(red: CGFloat(Float.random(in: 0...1)), green: CGFloat(Float.random(in: 0...1)), blue: CGFloat(Float.random(in: 0...1)), alpha: CGFloat(Float.random(in: 0.1 ... 1.0)))
+        UIView.animate(withDuration: 1) {
+            self.roundButton.frame.origin.x = CGFloat(self.currentX)
+            self.roundButton.frame.origin.y = CGFloat(self.currentY)
+            
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundButton.layer.masksToBounds = true
+        roundButton.layer.cornerRadius = roundButton.frame.width/2
         
-        UIView.animate(withDuration: 3, animations: {
-            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 150, height: 100)
-            self.viewFrame.backgroundColor = UIColor.blue
-        })
-        
-        UIView.animate(withDuration: 1, delay: 3, animations: {
+//
+//        UIView.animate(withDuration: 3, animations: {
+//            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 150, height: 100)
+//            self.viewFrame.backgroundColor = UIColor.blue
+//        })
+//
+//        UIView.animate(withDuration: 2, delay: 1, animations: {
 //          self.viewFrame.center.x = CGFloat(80)
-            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 80, height: 50)
-            self.viewFrame.backgroundColor = UIColor.red
-            self.viewFrame.center = CGPoint(x: 180, y: 200)
-        })
+//            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 100, height: 100)
+//            self.viewFrame.backgroundColor = UIColor.red
+//            self.viewFrame.center = CGPoint(x: 100, y: 300)
+//            // opacity = alpha (range 0 sampai 1) 0 paling blur, 1 paling jelas
+//            self.viewFrame.alpha = 0.2
+//        })
     }
 
 
