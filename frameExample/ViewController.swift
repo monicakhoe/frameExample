eabf31bba57e1649346e7c1d663586a59fb8c302
monicakhoe <monickhoe@yahoo.com>
2019-05-17 15:09:15 +0700

    Added Background color, delay, & center

--- frameExample/ViewController.swift
+++ frameExample/ViewController.swift
@@ -18,8 +18,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         UIView.animate(withDuration: 3, animations: {
-            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 80, height: 50)
+            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 150, height: 100)
             self.viewFrame.backgroundColor = UIColor.blue
+        })
+        
+        UIView.animate(withDuration: 1, delay: 3, animations: {
+//          self.viewFrame.center.x = CGFloat(80)
+            self.viewFrame.frame = CGRect(x: 150, y: 200, width: 80, height: 50)
+            self.viewFrame.backgroundColor = UIColor.red
+            self.viewFrame.center = CGPoint(x: 180, y: 200)
         })
     }
 
